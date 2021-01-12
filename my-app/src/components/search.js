ebecf9e78e7a95865242f90c35d74cf6a33d91c7
Chandra Prakash <cptiwari20@gmail.com>
2021-01-12 12:58:33 +0530

    added search errors

--- my-app/src/components/search.js
+++ my-app/src/components/search.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 export default ({getResults}) => {
     const [searchString, setSearchString] = useState('');
     const [seachType, setSeachType] = useState('opportunities');
-    const [results, setSetResults] = useState([]);
+    const [results, setResults] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
 
     const handleSearch = (e) => {
+        if(!searchString) return setError('Please add some charachers to search!')
         setLoading(true)
         const url = `https://search.torre.co/${seachType}/_search/?offset=0&size=10&aggregate=true&lang=en`
         fetch(url, {
@@ -17,11 +19,14 @@ export default ({getResults}) => {
             body: JSON.stringify({name: { term: searchString}})
         }).then(async (res) => {
             let data = await res.json()
-            setSetResults(data.results)
             getResults({
                 seachType,
                 results: data.results
             });
+            setResults(data.results)
+            if(data.results.length === 0){
+                setError('No results found! Please search again!')
+            }
             setLoading(false)
         }).catch((e) => {   
             console.log(e)
@@ -29,16 +34,30 @@ export default ({getResults}) => {
         })
     }
 
+    const handleValueChange = (value, key) => {
+        setError('')
+        setLoading(false)
+        setResults([])
+        switch (key) {
+            case 'string':
+                return setSearchString(value)
+            case 'type':
+                return setSeachType(value)
+            default:
+                break;
+        }
+    }
+
     return <div className={results.length !== 0 ? "section-container-left": "section-container-center"}>
       <div className="section-search">
         <h1 className="heading-main fw-bold">Find Jobs & People.</h1>
         <input 
-            onChange={e => setSearchString(e.target.value)} 
+            onChange={e => handleValueChange(e.target.value, 'string')} 
             className="input-element"
             type="text" 
             placeholder="Enter the search term" />
         <select 
-            onChange={e => setSeachType(e.target.value)}
+            onChange={e => handleValueChange(e.target.value, 'type')}
             className="input-element">
             <option value="opportunities">Jobs</option>
             <option value="people">People</option>
@@ -46,6 +65,7 @@ export default ({getResults}) => {
         <button onClick={handleSearch} className="input-element btn-search fw-bold">
             {loading ? "Loading..." : "Find"}  
         </button>
+        {error && <p className='error'>{error}</p>}
       </div>
     </div>   
 }
